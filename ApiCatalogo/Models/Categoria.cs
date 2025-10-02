3d5b9d56eac2b604431e088869a4ba6d39022700
Natal Lima <nataldelima@gmail.com>
2025-10-02 11:07:56 -0400

    Projeto inicial

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,8 @@
+namespace ApiCatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+}
