5bcf0b7ca2b251db116fbb00c156b0ba9bb3045a
Natal Lima <nataldelima@gmail.com>
2025-10-03 20:07:45 -0400

    Criado banco de dados

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,8 +1,26 @@
-namespace ApiCatalogo.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic;
 
+namespace ApiCatalogo.Models;
+[Table("Categorias")]
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
     public int CategoriaId { get; set; }
+    
+    [Required]
+    [StringLength(80)]
+    
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
+    
     public string? ImageUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
