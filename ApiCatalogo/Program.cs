5f2c66483b6e26ce91774c67e3c9cf9ac660d2f8
Natal Lima <nataldelima@gmail.com>
2025-10-06 20:23:07 -0400

    Corrigindo problema de serialização

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using ApiCatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,13 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
